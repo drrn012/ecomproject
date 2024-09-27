6c39fc6f6a3e08a12149844d18d0f5987696b0e0
drrn012 <pandianderensh@gmail.com>
2024-09-27 19:38:32 +0700

    test

--- main/models.py
+++ main/models.py
@@ -10,3 +10,14 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+
+'''
+class Person(models.Models):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField(max=100)
+    is_happy= models.BooleanField()
+
+    def __str__(self):
+        return self.name 
+'''
+
