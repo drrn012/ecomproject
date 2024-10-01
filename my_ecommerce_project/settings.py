69045c3d11d41ea86ca845f28d1161f8a2b2216f
drrn012 <pandianderensh@gmail.com>
2024-10-01 12:08:09 +0700

    whitenoise again

--- my_ecommerce_project/settings.py
+++ my_ecommerce_project/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic'
     'main'
     
 ]
@@ -53,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'my_ecommerce_project.urls'
